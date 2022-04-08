ff7d8a9569dda433c38ce6ab74536acc75db4b36
Max van Dorst <mrhvandorst@gmail.com>
2022-04-08 16:17:38 +0200

    pagina aangemaakt

---
+++ src/home/Home.jsx
@@ -0,0 +1,14 @@
+import { AcUnit } from "material-ui/icons"
+import "./home.scss"
+
+
+const Home = () => {
+  return (
+    <div className="home">
+      <AcUnit/>
+    </div>
+    
+  )
+}
+
+export default Home
