6402f22cb23786c36077c6854a66a6eede7d9d41
Max van Dorst <mrhvandorst@gmail.com>
2022-04-08 20:54:50 +0200

    hover van de lists, de lists gevuld, en description etc toegevoed.

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -7,27 +7,47 @@ import ListItem from "../listItem/ListItem";
 import "./list.scss";
 
 export default function List() {
+    const [slideNumber, setSlideNumber] = useState(false);
+    const [isMoved, setIsMoved] = useState(0);
+
+    const listRef = useRef()
+
+    const handleClick = (direction) => {
+        setIsMoved(true);
+        let distance = listRef.current.getBoundingClientRect().x - 50;
+        if (direction === "left" && slideNumber > 0) {
+            setSlideNumber(slideNumber - 1);
+            listRef.current.style.transform = `translateX(${230 + distance}px)`;
+        }
+        if (direction === "right" && slideNumber < 4) {
+            setSlideNumber(slideNumber + 1);
+            listRef.current.style.transform = `translateX(${-230 + distance }px)`;
+            
+        }
+        console.log(distance);
+    }
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="sliderArrow left"
-          // onClick={() => handleClick("left")}
-          // style={{ display: !isMoved && "none" }}
+        <ArrowBackIosOutlined 
+            className="sliderArrow left" 
+            onClick={()=>handleClick("left")}
+            style={{ display: !isMoved && "none" }}
         />
-        <div className="container">
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+        <div className="container" ref={listRef}>
+          <ListItem index={0}/>
+          <ListItem index={1}/>
+          <ListItem index={2}/>
+          <ListItem index={3}/>
+          <ListItem index={4}/>
+          <ListItem index={5}/>
+          <ListItem index={6}/>
+          <ListItem index={7}/>
+          <ListItem index={8}/>
+          <ListItem index={9}/>
         </div>
-        <ArrowForwardIosOutlined className="sliderArrow right"
+        <ArrowForwardIosOutlined className="sliderArrow right" onClick={()=>handleClick("right")}
           // onClick={() => handleClick("right")}
         />
       </div>
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,11 +1,40 @@
 import "./listItem.scss";
+import {PlayArrow, Add, ThumbUpAltOutlined, ThumbDownOutlined} from "@material-ui/icons"
+import { useState } from "react"
 
-
-export default function ListItem () {
+export default function ListItem ({index}) {
+  const [isHovered,SetIsHovered] = useState(false);
 
   return (
-    <div className="listItem">
-      item
+    <div className="listItem" 
+    style={{left: isHovered && index * 225}}
+    onMouseEnter={()=>SetIsHovered(true)} 
+    onMouseLeave={()=>SetIsHovered(false)}>
+      <img src="https://wallpaperaccess.com/full/6993439.png" alt="" />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow/>
+          <Add/>
+          <ThumbUpAltOutlined/>
+          <ThumbDownOutlined/>
+        </div>
+        <div className="itemInfoTop">
+          <span>1 Hour 14 mins</span>
+          <span className="limit">+16</span>
+          <span>2020</span>
+          <div className="desc">
+            Lorem ipsum dolor sit amet,
+             consectetur adipisicing elit. Ex autem esse non nisi, minima maiores 
+             aliquam. 
+             Laborum dicta ex enim amet ea sunt aperiam iusto? Nostrum quam quaerat voluptas nobis.
+          </div>
+          <div className="gerne">
+          Action
+          </div>
+          
+        </div>
+      </div>
     </div>
+    
   );
 }
