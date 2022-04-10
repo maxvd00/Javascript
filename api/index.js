d15248978c4240b78820d51d19cc64a291a7e4aa
Max van Dorst <mrhvandorst@gmail.com>
2022-04-10 15:37:08 +0200

    api werkt alleen niet

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose"); // import van MONGO_DB
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+//connect met MONGODB
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connectie is gelukt"))
+  .catch((err) => { console.log(err);
+  });
+
+  app.use(express.json()); // accepteerd json files
+
+  app.use("/api/auth", authRoute);
+
+
+app.listen(8800, () => {
+  console.log("Backend server is aan het runnen");
+});
